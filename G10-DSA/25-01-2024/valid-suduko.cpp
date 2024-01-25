89ec5e236f9961ae44bd3b14e61f9ed5cace0e64
codeshubham <shubhammank@gmail.com>
2024-01-25 11:51:35 +0530

    2d matrix

--- G10-DSA/25-01-2024/valid-suduko.cpp
+++ G10-DSA/25-01-2024/valid-suduko.cpp
@@ -2,16 +2,27 @@
 using namespace std;
 
 int main(){
-	int a = {
-		{8,3,0,0,7,0,0,0,0}
+	int a[9][9] = {
+		{9,3,0,0,7,0,0,0,0}
 		,{6,0,0,1,9,5,0,0,0}
-		,{0,9,8,0,0,0,0,6,0}
-		,{8,0,0,0,6,0,0,0,3}
+		,{0,7,8,0,0,0,0,6,0}
+		,{5,0,0,0,6,2,0,0,3}
 		,{4,0,0,8,0,3,0,0,1}
 		,{7,0,0,0,2,0,0,0,6}
 		,{0,6,0,0,0,0,2,8,0}
 		,{0,0,0,4,1,9,0,0,5}
 		,{0,0,0,0,8,0,0,7,9}
+	}
+
+	for(int row=0;row<9;row++){
+		for(int col=0;col<9;col++){
+			int ele = a[row][col];
+			// check if it repeats in row
+
+			// check if it repeats in col
+
+			// check if it repeats in sub-matrix 
+		}
 	}
 	return 0;
 }
